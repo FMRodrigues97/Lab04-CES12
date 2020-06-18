1fe74d5f9812d281ef3f083a6f2a50e75ae99835
FMRodrigues97 <63131709+FMRodrigues97@users.noreply.github.com>
2020-06-18 17:01:05 -0300

    Atualização dos gráficos

--- Lab04.m
+++ Lab04.m
@@ -5,60 +5,65 @@ PD = csvread('troco_PD_BR200.csv');
 GRL = csvread('troco_GR_GRLoses.csv');
 PDL = csvread('troco_PD_GRLoses.csv');
 
-%% DivisÃ£o e Conquista (DC)
+%% Divisão e Conquista (DC)
 
 % Figura 1
 figure;
 plot (DC(1:40, 1), DC(1:40, 2));
 xlabel ('Valor do Troco');
-ylabel ('Tempo de execuÃ§Ã£o (ms)');
-title ('Tempo de execuÃ§Ã£o para o mÃ©todo DC');
+ylabel ('Tempo de execução (ms)');
+title ('Tempo de execução para o método DC');
 grid on;
+print -dpng -r400 1.png;
 
 % Figura 2
 figure;
 plot (DC(1:40, 1), DC(1:40, 3));
 xlabel ('Valor do Troco');
-ylabel ('NÃºmero de Moedas');
-title ('NÃºmero de moedas para o mÃ©todo DC');
+ylabel ('Número de Moedas');
+title ('Número de moedas para o método DC');
 grid on;
+print -dpng -r400 2.png;
 
-
-%% MÃ©todo Greedy (GR)
+%% Método Greedy (GR)
 
 % Figura 3
 figure;
 plot (GR(1:200, 1), GR(1:200, 2));
 xlabel ('Valor do Troco');
-ylabel ('Tempo de execuÃ§Ã£o (ms)');
-title ('Tempo de execuÃ§Ã£o para o mÃ©todo GR');
+ylabel ('Tempo de execução (ms)');
+title ('Tempo de execução para o método GR');
 grid on;
+print -dpng -r400 3.png;
 
 % Figura 4
 figure;
 plot (GR(1:200, 1), GR(1:200, 3));
 xlabel ('Valor do Troco');
-ylabel ('NÃºmero de Moedas');
-title ('NÃºmero de moedas para o mÃ©todo GR');
+ylabel ('Número de Moedas');
+title ('Número de moedas para o método GR');
 grid on;
+print -dpng -r400 4.png;
 
-%% ProgramaÃ§Ã£o DinÃ¢mica (PD)
+%% Programação Dinâmica (PD)
 
 % Figura 5
 figure;
 plot (PD(1:200, 1), PD(1:200, 2));
 xlabel ('Valor do Troco');
-ylabel ('Tempo de execuÃ§Ã£o (ms)');
-title ('Tempo de execuÃ§Ã£o para o mÃ©todo PD');
+ylabel ('Tempo de execução (ms)');
+title ('Tempo de execução para o método PD');
 grid on;
+print -dpng -r400 5.png;
 
 % Figura 6
 figure;
 plot (PD(1:200, 1), PD(1:200, 3));
 xlabel ('Valor do Troco');
-ylabel ('NÃºmero de Moedas');
-title ('NÃºmero de moedas para o mÃ©todo PD');
+ylabel ('Número de Moedas');
+title ('Número de moedas para o método PD');
 grid on;
+print -dpng -r400 6.png;
 
 %% Comparativo GR e PD (loses)
 
@@ -68,7 +73,33 @@ hold on;
 plot (GRL(1:200, 1), GRL(1:200, 3), 'black');
 plot (PDL(1:200, 1), PDL(1:200, 3), 'r--');
 xlabel ('Valor do Troco');
-ylabel ('NÃºmero de Moedas');
+ylabel ('Número de Moedas');
 title ('Comparativo GR e PD');
-legend ('Greedy', 'ProgramaÃ§Ã£o DinÃ¢mica');
+legend ('Greedy', 'Programação Dinâmica');
 grid on;
+print -dpng -r400 7.png;
+
+%% Recursão
+
+figure;
+plot (DC(1:40, 1), DC(1:40, 4));
+xlabel ('Valor do Troco');
+ylabel ('Chamadas Recursivas');
+title ('Chamadas Recursivas para o método DC');
+grid on;
+print -dpng -r400 8.png;
+
+%% Comparativo
+
+figure;
+hold on;
+plot (GR(1:200, 1), GR(1:200, 2));
+plot (GRL(1:200, 1), GRL(1:200, 2));
+plot (PD(1:200, 1), PD(1:200, 2));
+plot (PDL(1:200, 1), PDL(1:200, 2));
+xlabel ('Valor do Troco');
+ylabel ('Tempo de Execução (ms)');
+title ('Comparativo GR e PD');
+legend ('GR', 'GRL', 'PD', 'PDL');
+grid on;
+print -dpng -r400 9.png;
