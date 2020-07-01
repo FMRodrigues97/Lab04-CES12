efd42048497d67203fc87c1e13387fedfa84e85f
FMRodrigues97 <63131709+FMRodrigues97@users.noreply.github.com>
2020-07-01 14:52:24 -0300

    SSP - PD

--- SSPSolverPD.cpp
+++ SSPSolverPD.cpp
@@ -1,91 +1,68 @@
 #include <SubsetSumSolver.h>
 
-bool SSPSolverPD::solve(const std::vector< long> &input, long value, std::vector< char> &output) {
+bool SSPSolverPD::solve(const std::vector<long> &input, long value, std::vector<char> &output) {
 
-    long n = input.size();
-    bool solucionavel;
-
-    Knapsack01() {
-        for (int i = 0; i <= c; i++)
-            B[0][i] = 0;
-        
-        for (int k = 1; k <= n; k++)
-            for (i = 0; i <= c; i++)
-                if (w[k] > i)
-                    B[k][i] = B[k - 1][i];
-                else 
-                    B[k][i] = max{ B[k - 1][i], B[k - 1][i - w[k]] + p[k] };
-
-        return B[n][c];
-     }
-
+    long i = 0, k = 0, indice = 0, n = input.size();
+    bool solucionavel = false;
+    std::vector<std::vector<long>> B;
+    B.resize(input.size());
     output.resize(input.size());
-    return solucionavel;
-    
-}
 
-bool SSPSolverPD::solve(const std::vector< long> &input, long value, std::vector< char> &output) {
+    while (indice < n)
+        output[indice++] = false;
 
-    long w = 0, i = 0;
-    bool encontrouSol = false;
-    std::vector<std::vector<long>> tabela;
-    output.resize(input.size());
-    tabela.resize(input.size());
+    for (i = input.size() - 1; i >= 0; i--)
+        B[i].resize(value + 1, 0); // Nenhum item é considerado
 
-    long index = 0,n = input.size();
-
-    while (index < n)
-        output[index++] = false;
-
-    for (int i=input.size() - 1;i >= 0; i--)
-        tabela[i].resize(value+1,0);
-
-    i = 0;
-    for(long n = input.size(); i < n && (!encontrouSol); i++) {
-        w = 0;
-        for(; w <= value && (!encontrouSol); w++) {
-            if (input[i]<=w) {
-                if (i==0)
-                    tabela[i][w] = input[i];
-
-                else if (input[i] + tabela[i-1][w-input[i]] >= tabela[i-1][w])
-                    tabela[i][w] = input[i] + tabela[i-1][w-input[i]];
+    for (k = 0; k < n; k++) { // Incremento nos itens
+        for (i = 0; i <= value; i++) { // Incremento na capacidade
+            if (input[k] > i) {
+                if (k == 0)
+                    B[k][i] = 0;
                 else
-                    tabela[i][w] = tabela[i-1][w];
-                
+                    B[k][i] = B[k-1][i];
             }
-            else
-                if (i != 0)
-                    tabela[i][w] = tabela[i-1][w];
-                else
-                    tabela[i][w] = 0;
 
-                // tabela[i][w] = (i!=0?tabela[i-1][w]:0);
+            else {
+                if (k != 0){
+                    if (B[k-1][i-input[k]] + input[k] > B[k-1][i])
+                        B[k][i] = B[k-1][i-input[k]] + input[k];
 
-            if(tabela[i][w] == value)
-                encontrouSol = true;
+                    if (B[k-1][i] >= B[k-1][i-input[k]] + input[k])
+                        B[k][i] = B[k-1][i];
+                }
+                
+                if (k == 0)
+                    B[k][i] = input[k];
+            }
+
+            if (B[k][i] == value)
+                solucionavel = true;
         }
     }
 
+    k--;
     i--;
-    w--;
 
-    if (encontrouSol)
-        while(i >= 0 && w > 0) {
-            if (i > 0) {
-                if (tabela[i][w] != tabela[i-1][w]) {
-                    output[i] = true;
-                    w -= input[i];
+    if (solucionavel){
+        while (k >= 0 && i > 0) {
+            if (k > 0) {
+                if (B[k][i] != B[k-1][i]) {
+                    output[k] = true;
+                    i -= input[k];
                 }
-                i--;
             }
+
             else {
-               if (tabela[i][w] == input[i]) {
-                    output[i] = true;
-                    w -= input[i];
-                }
-                i--; 
+                if (B[k][i] == input[k]) {
+                        output[k] = true;
+                        i -= input[k];
+                    }
             }
+
+            k--;
         }
-    return encontrouSol;
+    }
+
+    return solucionavel;
 }
